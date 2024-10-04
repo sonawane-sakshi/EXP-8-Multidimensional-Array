708695c477018e9d859273e00c6587495222ea46
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-04 23:04:59 +0530

    Update 2D Array Basics.cpp

--- 2D Array Basics.cpp
+++ 2D Array Basics.cpp
@@ -26,3 +26,18 @@ int main() {
 
     return 0;
 }
+
+/*
+**output**
+    Enter number of rows:
+2
+Enter number of columns:
+2
+Enter element at position (1, 1): 4
+Enter element at position (1, 2): 7
+Enter element at position (2, 1): 8
+Enter element at position (2, 2): 5
+The array elements are:
+4 7
+8 5
+*/
